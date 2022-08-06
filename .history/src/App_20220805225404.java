4a6e54eaacc340481ccc5fb85595707877ba2dca
tive02 <jsmorenop@unal.edu.co>
2022-08-05 23:16:37 -0500

    Conexion BD

--- .history/src/App_20220805225404.java
+++ .history/src/App_20220805225404.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         var reportesView = new ReportesView();
-        var banco = "Conavi";
-        reportesView.proyectosFinanciadosPorBanco(banco);
+var banco = "Conavi";
+reportesView.proyectosFinanciadosPorBanco(banco)
     }
 }
--- .history/src/App_20220805225414.java
+++ .history/src/App_20220805225414.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         var reportesView = new ReportesView();
         var banco = "Conavi";
-        reportesView.proyectosFinanciadosPorBanco(banco);
+        reportesView.proyectosFinanciadosPorBanco(banco)
     }
 }
